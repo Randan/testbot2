b4f35f5ab488a6e246fd4e05d7d5dd82c5204d82
Andrew Randan <andrewrandan@gmail.com>
2020-06-12 18:06:54 +0300

    temporary remove mongo

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const router = require('./routes/routes');
 const logging = require('./middlewares/logging');
 
-mongoose.connect(
-    process.env.DATABASE_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
-const db = mongoose.connection;
+// mongoose.connect(
+//     process.env.DATABASE_URL,
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+// );
+// const db = mongoose.connection;
 
-db.on('error', error => console.log(error));
-db.once('open', () => console.log('connected to database'));
+// db.on('error', error => console.log(error));
+// db.once('open', () => console.log('connected to database'));
 
 app.use(express.json());
 app.use(logging);
 
-app.use(bot);
+app.use(router);
 
 app.listen(process.env.APP_PORT, () => console.log('server started'));
