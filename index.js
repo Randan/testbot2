06b6afebce680f16a557c80611ff38f8b338a889
Andrew Randan <andrewrandan@gmail.com>
2020-06-12 18:34:02 +0300

    reverted

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
+process.env["NTBA_FIX_319"] = 1; // Fix of 319 error
+
 require('dotenv').config();
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
 
-const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
+const bot = new TelegramBot(process.env.BOT_API, { polling: true });
 
-const router = require('./routes/routes');
-const logging = require('./middlewares/logging');
+bot.on('message', msg => {
+  const { from, chat } = msg;
+  username === 'randan'
+    && bot.sendMessage(
+      chat.id,
+      `Ты пидор, ${from.first_name}!`
+    );
+})
+// bot.onText(/\/help/, msg => {
+//   const { id, first_name, last_name, username } = msg.from;
 
-// mongoose.connect(
-//     process.env.DATABASE_URL,
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-// );
-// const db = mongoose.connection;
+//   bot.sendMessage(
+//     id,
+//     `Hello, ${first_name}! And Fuck you!\n`
+//   );
+// });
 
-// db.on('error', error => console.log(error));
-// db.once('open', () => console.log('connected to database'));
+// bot.onText(/\/whoAmI/, msg => {
+//   const { id, first_name, last_name, username } = msg.from;
 
-app.listen(APP_PORT, () => console.log(`Server works on ${PORT}`));
+//   bot.sendMessage(
+//     id,
+//     `Hello, ${first_name}! And Fuck you!\n`
+//     + `You are\n`
+//     + `${id}\n`
+//     + `${first_name}\n`
+//     + `${last_name}\n`
+//     + `${username}\n`
+//   );
+// });
+
+// bot.onText(/\/buttons/, function (msg) {
+//   const opts = {
+//     reply_to_message_id: msg.message_id,
+//     reply_markup: JSON.stringify({
+//       keyboard: [
+//         ['first button'],
+//         ['second button']
+//       ]
+//     })
+//   };
+//   bot.sendMessage(msg.chat.id, 'Buttons?', opts);
+// });
+
+app.listen(process.env.APP_PORT, () => console.log(`Server works on ${process.env.APP_PORT}`));
