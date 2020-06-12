9873f5ec603b87af946a6e361250ea1c598ed0dd
Andrew Randan <andrewrandan@gmail.com>
2020-06-13 01:11:52 +0300

    null -> "Markdown"

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ const bot = new TelegramBot(process.env.BOT_API, { polling: true });
 bot.on('message', msg => {
   const { from, chat } = msg;
 
-  // from.id === process.env.ABUSE_TARGET_ID && bot.sendMessage(chat.id, getAbusePhrase(), null, null, true);
+  // from.id === process.env.ABUSE_TARGET_ID && bot.sendMessage(chat.id, getAbusePhrase(), 'Markdown', null, true);
 
-  bot.sendMessage(process.env.ROOT_ID, `${from.first_name} ${from.last_name} (@${from.username}) [${from.id}]`, null, null, true);
+  bot.sendMessage(process.env.ROOT_ID, `${from.first_name} ${from.last_name} (@${from.username}) [${from.id}]`, 'Markdown', null, true);
 });
 
 bot.on('polling_error', msg => console.log(msg));
 
 app.listen(process.env.APP_PORT, () => console.log(`Server works on ${process.env.APP_PORT}`));
 
-cron.schedule('10 1 * * *', () => {
-  bot.sendMessage(`${process.env.CHAT_ID}`, '/pidor@SublimeBot', null, null, true);
+cron.schedule('14 1 * * *', () => {
+  bot.sendMessage(`${process.env.CHAT_ID}`, '/pidor@SublimeBot', 'Markdown', null, true);
 }, {
   scheduled: true,
   timezone: process.env.TIMEZONE
