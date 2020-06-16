66284adb7a53b332f92ad5ad7cf3211adb75afcc
Andrew Randan <andrewrandan@gmail.com>
2020-06-16 18:18:19 +0300

    updated phrases

--- getFuck9GAGPhrases.js
+++ getFuck9GAGPhrases.js
@@ -2,7 +2,10 @@ const phrasesArr = [
   'Опять 9GAG?',
   'Та ты заебал',
   'Ну хватит',
-  'Ты ж в курсе, что оно не подгружает?'
+  'Ты ж в курсе, что оно не подгружает?',
+  'МММ, Жоско!',
+  '9GAG хуйня',
+  'Иди умойся'
 ];
 
 module.exports = () => phrasesArr[Math.floor(Math.random() * (phrasesArr.length))];
