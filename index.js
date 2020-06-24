2da7a00d21989ca7dbc4e19724241467dd47c827
Andrew Randan <andrewrandan@gmail.com>
2020-06-24 15:11:36 +0300

    added GET / request

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(req, res) {
+  res.send('This is a Zhossko Telegram Bot');
+});
+
 require('./events')
 
 app.listen(process.env.APP_PORT, () => console.log(`Server works on ${process.env.APP_PORT}`));
